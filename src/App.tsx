b4b0193647d13c510449244f9993bac2896895f0
Jacques Lucke <mail@jlucke.com>
2024-12-24 23:39:23 +0100

    local storage test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+
 function App() {
-  return <p className="text-2xl">Initial workout page</p>;
+  const [todos, setTodos] = useLocalStorageState<string[]>("todos", {
+    defaultValue: [],
+  });
+
+  const [query, setQuery] = useState("");
+
+  function onClick() {
+    setTodos([...todos, query]);
+    setQuery("");
+  }
+
+  return (
+    <>
+      <input value={query} onChange={(e) => setQuery(e.target.value)} />
+      <button onClick={onClick}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
